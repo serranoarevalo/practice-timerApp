7eb90a4b944cbee0ece5f21a9f815dc59b31e864
fullyalive <wogud1202@gmail.com>
2018-05-10 05:07:06 +0900

    Turning seconds into minutes

--- concentration-timer/components/Timer/presenter.js
+++ concentration-timer/components/Timer/presenter.js
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 import { View, Text, StyleSheet, StatusBar } from "react-native";
 import Button from "../Button";
 
+function formatTime(time) {
+    let minutes = Math.floor(time / 60);
+    time -= minutes * 60;
+    let seconds = parseInt(time % 60, 10);
+    return `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10
+        ? `0${seconds}`
+        : seconds}`;
+ 
+    return;
+}
+
 class Timer extends Component {
     componentWillReceiveProps(nextProps) {
         const currentProps = this.props;
@@ -28,7 +39,7 @@ class Timer extends Component {
         const {
             isPlaying,
             elapsedTime,
-            timeDuration,
+            timerDuration,
             startTimer,
             restartTimer,
             addSecond
@@ -37,7 +48,8 @@ class Timer extends Component {
             <View style={styles.container}>
                 <StatusBar barStyle="light-content" />
                 <View style={styles.upper}>
-                    <Text style={styles.time}>25:00</Text>
+                    <Text style={styles.time}>
+                    {formatTime(timerDuration - elapsedTime)}</Text>
                 </View>
                 <View style={styles.lower}>
                     {!isPlaying && (
