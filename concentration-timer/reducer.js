af7fd352226803e51ab3f2c27b293c62b9fa3df4
fullyalive <wogud1202@gmail.com>
2018-05-11 15:09:38 +0900

    try to reset timer

--- concentration-timer/reducer.js
+++ concentration-timer/reducer.js
@@ -2,28 +2,28 @@
 
 // Actions
 
-const START_TIMER = 'START_TIMER';
-const RESTART_TIMER = 'RESTART_TIMER';
-const ADD_SECOND = 'ADD_SECOND';
+const START_TIMER = "START_TIMER";
+const RESTART_TIMER = "RESTART_TIMER";
+const ADD_SECOND = "ADD_SECOND";
 
 // Action Creators
 
 function startTimer() {
-    return {
-        type: START_TIMER
-    };
+  return {
+    type: START_TIMER
+  };
 }
 
 function restartTimer() {
-    return {
-        type: RESTART_TIMER
-    };
+  return {
+    type: RESTART_TIMER
+  };
 }
 
 function addSecond() {
-    return {
-        type: ADD_SECOND
-    };
+  return {
+    type: ADD_SECOND
+  };
 }
 
 // Reducer
@@ -32,64 +32,64 @@ const TIMER_DURATION = 3;
 const TIMER_COUNT = 0;
 
 const initialState = {
-    isPlaying: false,
-    elapsedTime: 0,
-    timerDuration: TIMER_DURATION,
-    timerCount: TIMER_COUNT
+  isPlaying: false,
+  elapsedTime: 0,
+  timerDuration: TIMER_DURATION,
+  timerCount: TIMER_COUNT
 };
 
 function reducer(state = initialState, action) {
-    switch (action.type) {
-        case START_TIMER:
-            return applyStartTimer(state, action);
-        case RESTART_TIMER:
-            return applyRestartTimer(state);
-        case ADD_SECOND:
-            return applyAddSecond(state);
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case START_TIMER:
+      return applyStartTimer(state, action);
+    case RESTART_TIMER:
+      return applyRestartTimer(state);
+    case ADD_SECOND:
+      return applyAddSecond(state);
+    default:
+      return state;
+  }
 }
 
 // Reducer Functions
 
 function applyStartTimer(state) {
-    return {
-        ...state,
-        isPlaying: true
-    };
+  return {
+    ...state,
+    isPlaying: true
+  };
 }
 
 function applyRestartTimer(state) {
+  return {
+    ...state,
+    isPlaying: false,
+    elapsedTime: 0
+  };
+}
+
+function applyAddSecond(state, action) {
+  const { elapsedTime } = state;
+  if (elapsedTime < TIMER_DURATION) {
     return {
-        ...state,
-        isPlaying: false,
-        elapsedTime: 0
+      ...state,
+      elapsedTime: elapsedTime + 1
     };
+  } else {
+    return {
+      ...state,
+      isPlaying: false
+      // timerCount: state.timerCount + 1,
+    };
+  }
 }
-
-function applyAddSecond(state) {
-    if (state.elapsedTime < TIMER_DURATION) {
-        return {
-            ...state,
-            elapsedTime: state.elapsedTime + 1
-        };
-    } else {
-        return {
-            ...state,
-            isPlaying: false,
-            timerCount: state.timerCount + 1,
-        };
-    }
-}
-
 
 // Export Action Creators
 
 const actionCreators = {
-    startTimer,
-    restartTimer,
-    addSecond,
+  startTimer,
+  restartTimer,
+  addSecond
 };
 
 export { actionCreators };
